e25f84512a41020adce32f512f5203cef3280c32
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-10-12 17:40:55 +0200

    Stap 3 - Show me something
    reworked the code to show the  required information

--- S3 exercises/Time Wasted Application/parasite.js
+++ S3 exercises/Time Wasted Application/parasite.js
@@ -1,5 +1,39 @@
+window.onload = function(){
+    searchMovie();
+}
 
-fetch("http://www.omdbapi.com/?i=tt6751668&apikey=2c9d6532")
+function searchMovie(){
+
+    fetch("http://www.omdbapi.com/?t=MADAGASCAR&apikey=2c9d6532")
 .then(response => response.json())
-.then(data=>console.log(data))
+.then(data=>{
+    console.log(data);
+    console.log(data.Title);
+    showMovie(data)
+})
+}
+
+function showMovie(data){
+    let movie = 
+    `
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img src="${data.Poster}" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">${data.Title}</h5>
+                <p class="card-text">${data.Plot}</p>
+                <p class="card-text">${data.Director}</p>
+                <p class="card-text"><small class="text-muted">${data.Year}</small></p>
+                <p class="card-text"><small class="text-muted">${data.Runtime}</small></p>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+    document.getElementById('movieCard').innerHTML = movie;
+
+}
 
